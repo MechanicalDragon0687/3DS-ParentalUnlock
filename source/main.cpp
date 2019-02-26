165080d3c8f33330234c5f890e684086a7353086
MechanicalDragon <mechdragon@mechanicaldragon.xyz>
2019-02-25 18:47:16 -0800

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -10,7 +10,7 @@ PrintConsole topScreen, bottomScreen;
 using namespace std;
 
 void fucked() {
-	cout << "\n\nPress [Start] to exit";	
+	cout << "\n\nPress [Start] to exit\n\n";	
 	while (1) {
 		hidScanInput();
 		if (hidKeysDown() & KEY_START) { 
@@ -39,13 +39,14 @@ int main(int argc, char* argv[])
 		cout << "Failed to initialize APT services\n";
 		fucked();
 	}
+	cout << "Clearing Parental Controls";
+	CFGI_ClearParentalControls();
+
 	cout << "Initializing PM services\n";
 	if (R_FAILED(pmInit())) {
 		cout << "Failed to initialize PM services\n";
 		fucked();
-	}
-	cout << "Clearing Parental Controls";
-	CFGI_ClearParentalControls();
+	}	
 	u32 titlecount=0;
 	u64 titles[300];
 	u64 title=0;
