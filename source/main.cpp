008d551ea977b225bf145a99ff14b1c1c87fdfe0
MechanicalDragon <mechdragon@mechanicaldragon.xyz>
2019-02-28 14:02:49 -0800

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,9 @@
 
 #include <iostream>
-#include <iomanip>
-#include <vector>
 #include <string>
 #include <unistd.h>
 #include <3ds.h>
-#define SECOND(x) (x*1000ULL*1000ULL*1000ULL)
+
 PrintConsole topScreen, bottomScreen;
 using namespace std;
 
@@ -21,15 +19,6 @@ void fucked() {
 		}
 	}
 }
-void wait() {
-	//cout << "\n\nPress [Start] to exit\n\n";	
-	while (1) {
-		hidScanInput();
-		if (hidKeysDown() & KEY_START) { 
-			return;
-		}
-	}
-}
 
 int main(int argc, char* argv[])
 {
@@ -37,7 +26,7 @@ int main(int argc, char* argv[])
 	consoleInit(GFX_TOP, &topScreen);
 	consoleInit(GFX_BOTTOM, &bottomScreen);
 	consoleSelect(&bottomScreen);
-	bool isN3ds = false;
+
 	cout << "Initializing CFG services\n";
 	if (R_FAILED(cfguInit())) {
 		cout << "Failed to initialize CFG services\n";
@@ -48,47 +37,53 @@ int main(int argc, char* argv[])
 		cout << "Failed to initialize APT services\n";
 		fucked();
 	}
-	cout << "Clearing Parental Controls";
-//	CFGI_ClearParentalControls();
+
+	cout << "Allocating memory\n";
 	u8 ParentalSettings[0xc0] = {0};
 	u8 ParentalCOPPACS[0x14] = {0};
 	u8 ParentalEmail[0x200] = {0};
 	u8 ParentalConfig[0x94] = {0};
 
+	cout << "Reading current CONFIG data\n";
 	CFG_GetConfigInfoBlk8(0xc0, 0x00C0000, ParentalSettings);
 	CFG_GetConfigInfoBlk8(0x14, 0x00C0001, ParentalCOPPACS);
 	CFG_GetConfigInfoBlk8(0x200,0x00C0002, ParentalEmail);
 	CFG_GetConfigInfoBlk8(0x94, 0x0100001, ParentalConfig);
-	//ParentalSettings[0x00] = 0;
-	//ParentalSettings[0x01] = 0;
-	//ParentalSettings[0x02] = 0;
-	//ParentalSettings[0x03] = 0;
-//	ParentalSettings[0x03] = 0;
+
+	cout << "Clearing Parental Controls\n";
 	std::fill(ParentalSettings,ParentalSettings+0x10,0);
 	std::fill(ParentalConfig+0x0d,ParentalConfig+0x20,0);
 	std::fill(ParentalEmail,ParentalEmail+0x200,0);
 	std::fill(ParentalCOPPACS,ParentalCOPPACS+0x14,0);
+
+	cout << "Updating empty Parental Control settings\n";
 	if (R_FAILED(CFG_SetConfigInfoBlk8(0xc0, 0x00C0000, ParentalSettings))) {
 		cout << "Failed to reset Parental Control settings\n";
 	}
+
+	cout << "Updating empty COPPA settings\n";
 	if (R_FAILED(CFG_SetConfigInfoBlk8(0x14, 0x00C0001, ParentalCOPPACS))) {
 		cout << "Failed to reset Child Protection settings\n";
 	}
 
+	cout << "Updating empty Parent Email settings\n";
 	if (R_FAILED(CFG_SetConfigInfoBlk8(0x200, 0x00C0002, ParentalEmail))) {
 		cout << "Failed to reset Parent email\n";
 	}
 
+	cout << "Updating empty Parent secret question and pin\n";
 	if (R_FAILED(CFG_SetConfigInfoBlk8(0x94, 0x0100001, ParentalConfig))) {
 		cout << "Failed to set Parent secret question and pin settings\n";
 	}
 
+	cout << "Saving cleared settings\n";
 	if (R_FAILED(CFG_UpdateConfigSavegame())) {
 		cout << "Failed to update all settings\n";
 	}
  	cout << "\nDone!\n";
-	//CFG_SetConfigInfoBlk8(4, 0xD0000, eulaData);
 	fucked();
+	
+	// should never get here
 	amExit();
 	cfguExit();
 	gfxExit();
